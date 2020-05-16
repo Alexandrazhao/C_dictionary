22fbd1ab7e9f83feed5affd8f42e99461bc5fffa
KarolinaK96 <60366492+KarolinaK96@users.noreply.github.com>
2020-05-17 01:32:00 +0200

    Update DictionaryNull.java

--- DictionaryNull.java
+++ DictionaryNull.java
@@ -20,9 +20,11 @@ public class NullDictionary{
 			} */
 		File file = new File("macbeth-processed.txt");
 		Scanner input = new Scanner(file);
+		int n = 0;
 		while (input.hasNext()){
 			String line = input.nextLine();
 			String[] splitted = line.split(" ");
+			 //number of words in the dictionary
 			//System.out.println(splitted[0] + ":" + splitted[1]);
 			//System.out.println("Splitted length:" + splitted.length);
 			for(int i = 0; i < splitted.length; i++){
@@ -32,6 +34,7 @@ public class NullDictionary{
 				//String b = dictionary.get(splitted[i]);
 				//System.out.println(splitted[i] + " " + b);
 				//System.out.println("Number of words: " + i);
+				n = n + 1;
 			}
 			Arrays.fill(splitted, null);
 			
@@ -39,7 +42,17 @@ public class NullDictionary{
 		input.close();
 		long endTime = System.nanoTime();
 		long timeElapsed = endTime - startTime;
-		System.out.println("Execution time in miliseconds: " + (timeElapsed/1000000));
+		System.out.println("Time elasped of put in nanoseconds: " + (timeElapsed));
+		System.out.println("Average time of put per argument in nanoseconds: " + timeElapsed/n);
+		dictionary.put("apple", null);
+		dictionary.put("yes", null);
+		long startTimeGet = System.nanoTime();
+		System.out.println("dictionary.get(apple) = " + dictionary.get("apple"));
+		System.out.println("dictionary.get(yes) = " + dictionary.get("yes"));
+		long endTimeGet = System.nanoTime();
+		long timeElapsedGet = endTimeGet - startTimeGet;
+		System.out.println("Time elasped of get in nanoseconds: " + timeElapsedGet);
+		System.out.println("Average time of get per argument in nanoseconds: " + timeElapsedGet/2);
 		
 	}
 }
