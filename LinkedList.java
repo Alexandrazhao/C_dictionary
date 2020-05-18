d3ad4e8daac9fe8802ce468d718db24492bb5a4d
KarolinaK96 <60366492+KarolinaK96@users.noreply.github.com>
2020-05-19 00:19:59 +0200

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -3,43 +3,104 @@ import java.io.*;
 import java.lang.Object;
 
 public class LinkedList {
-    private static Node first; // top of stack - most recently added node
-    private int n; //number of nodes
-    private static class Node {
-        String key;
-        String value;
-        Node next;
-    }
-    
-     public static Node createNode(String key, String value) {
-    	Node oldfirst = first;
-    	first = new Node();
-    	first.key = key;
-    	first.value = value;
-    	return first;
-    }
-    public static void main(String args[]) throws FileNotFoundException{
-        System.out.print("I'm in main");
-        Stack<Node> dictionary = new Stack<Node>();
-        File file = new File("test.txt");
+	private Node first; // top of stack - most recently added node
+	static int n; // number of nodes
+
+	private static class Node {
+		String key;
+		String value;
+		Node next;
+	}
+
+	public Node createNode(String key, String value) {
+		Node oldfirst = first;
+		first = new Node();
+		first.key = key;
+		first.value = value;
+		first.next = oldfirst;
+		n = n + 1;
+		return first;
+	}
+	
+	public int size() {
+		return n;
+	}
+
+	public static void printNode(Node node) {
+		System.out.print("Node is: " + node.key + " " + node.value + "\n");
+	}
+
+	private static void searchForNode(Node node, Stack<Node> stack) {
+		for (int i = 0; i < stack.size(); i++) {
+			Node node1 = stack.get(i);
+			if (node1.key == node.key && node1.value == node.value) {
+				System.out.println("Node found: ");
+				printNode(node1);
+			} else {
+				continue;
+
+			}
+		}
+
+	}
+
+	private static void searchForNode(String key, Stack<Node> stack) {
+		for (int i = 1; i < stack.size(); i++) {
+			Node node1 = stack.get(i);
+			if (node1.key == key) {
+				printNode(node1);
+			} else {
+				continue;
+			}
+		}
+	}
+
+	public static void print(Stack<Node> stack) {
+		int i = 0;
+		while(i < stack.size()) {
+			System.out.println(stack.peek().key + " " + stack.peek().value);
+			stack.pop();
+			i = i + 1;
+			//System.out.println("i = " + i);
+		}
+	}
+
+	public static void main(String args[]) throws FileNotFoundException {
+		System.out.print("I'm in main\n");
+		Stack<Node> dictionary = new Stack<Node>();
+		LinkedList dict = new LinkedList();
+		File file = new File("macbeth-processed.txt");
 		Scanner input = new Scanner(file);
-		int n = 0; //number of words in dictionary
-		while (input.hasNext() && n < 5){
+		int m = 0; // number of words in dictionary
+		while (input.hasNext()) {
 			String line = input.nextLine();
 			String[] splitted = line.split(" ");
-			//System.out.println(splitted[0] + ":" + splitted[1]);
-			//System.out.println("Splitted length:" + splitted.length);
-			for(int i = 0; i < splitted.length; i++){
-				//System.out.println("i is:" + i);
-				//System.out.println("splitted[i] is:" + splitted[i]);
-				Node nowa = createNode(splitted[0], null);
+			// System.out.println(splitted[0] + ":" + splitted[1]);
+			// System.out.println("Splitted length:" + splitted.length);
+			for (int i = 0; i < splitted.length; i++) {
+				// System.out.println("i is:" + i);
+				// System.out.println("splitted[i] is:" + splitted[i]);
+				Node nowa = dict.createNode(splitted[i], null);
+				// printNode(nowa);
 				dictionary.push(nowa);
-				//String b = dictionary.get(splitted[i]);
-				//System.out.println(splitted[i] + " " + b);
+				//print(dictionary);
+				//int index = dictionary.search(splitted[i]);
+				//searchForNode(nowa, dictionary);
+				//System.out.println("index = " + index);
+				// System.out.println("The element is: " + dictionary.get(index));
+				// String b = dictionary.get(splitted[i]);
+				// System.out.println(splitted[i] + " " + b);
+				m = m + 1;
 			}
 			Arrays.fill(splitted, null);
-			n = n + 1;
+			
+			//print(dictionary);
+			//System.out.println("m= " + m);
+			//searchForNode("project", dictionary);
 		}
 		input.close();
+		searchForNode("project", dictionary);
+		
 	}
+
 }
